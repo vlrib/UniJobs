0bfa1b04c79230d00c413f15b1c56b676c4dfcad
felipe <felipetiagodecarli@hotmail.com>
2019-04-14 18:29:23 -0300

    fix now deployment config

--- backend/micro-unijobs-user/config.js
+++ backend/micro-unijobs-user/config.js
@@ -7,5 +7,14 @@ module.exports = {
     },
     systemKey: '1AEFCUWVXF0RZYJVOYHMHZCEAORADNBS',
     secret: 'secret'
+  },
+  production: {
+    auth: {
+      user: 1,
+      admin: 5,
+      system: 10
+    },
+    systemKey: '1AEFCUWVXF0RZYJVOYHMHZCEAORADNBS',
+    secret: 'secret'	
   }
 }
